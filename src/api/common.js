f4d8d51b152a4fc27985dc99653cbbdd931407f2
Nagy László Levente <nagylacika2002@gmail.com>
2023-08-27 19:21:38 +0200

    Trying to cover api

--- src/api/common.js
+++ src/api/common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const httpClient = axios.create({baseURL: "https://api.openweathermap.org/data/2.5/weather", headers: {}})
-const appId = "98923aeaac4b1a9b3aab3c265d98041b"
+const appId = process.env.API_ID
 
 export {httpClient, appId}
