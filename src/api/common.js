6b18e4b5a730d2d6753fd9dccc6206e1d9bfa494
Nagy László Levente <nagylacika2002@gmail.com>
2023-08-27 19:28:23 +0200

    Simply removed and changed key from GitHub

--- src/api/common.js
+++ src/api/common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const httpClient = axios.create({baseURL: "https://api.openweathermap.org/data/2.5/weather", headers: {}})
-const appId = process.env.API_ID
+const appId = ""
 
 export {httpClient, appId}
